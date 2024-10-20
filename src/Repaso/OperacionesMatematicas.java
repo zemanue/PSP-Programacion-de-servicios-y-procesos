132649d25a794df4cbb5526e27e19fa85f973cc2
zemanue <manureco.97@gmail.com>
2024-10-20 22:29:16 +0200

    Update OperacionesMatematicas.java
    Changed the return type for addition, substraction and multiplication

--- src/Repaso/OperacionesMatematicas.java
+++ src/Repaso/OperacionesMatematicas.java
@@ -1,13 +1,11 @@
 package Repaso;
 
 public interface OperacionesMatematicas {
-    public double sumar(int a, int b);
+    public int sumar(int a, int b);
 
-    public double restar(int a, int b);
+    public int restar(int a, int b);
 
-    public double multiplicar(int a, int b);
+    public int multiplicar(int a, int b);
 
     public double dividir(int a, int b);
-
-    // addition, subtraction, multiplication and division
 }
