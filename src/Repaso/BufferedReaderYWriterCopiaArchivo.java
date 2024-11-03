f67530e0fcb822b9b34d20242e97cccc7b04a9fd
zemanue <manureco.97@gmail.com>
2024-11-03 21:04:36 +0100

    Create BufferedReaderYWriterCopiaArchivo.java
    Created the class with its explanation

---
+++ src/Repaso/BufferedReaderYWriterCopiaArchivo.java
@@ -0,0 +1,16 @@
+
+// Ejercicio 1:
+// Desarrolla un programa que copie el contenido de un archivo de texto "original.txt" a un nuevo archivo "copia.txt". Asegúrate de que cada línea
+// se copie correctamente.
+
+package Repaso;
+
+import java.io.IOException;
+
+public class BufferedReaderYWriterCopiaArchivo {
+    public static void main(String[] args) throws IOException{
+
+
+
+    }
+}
