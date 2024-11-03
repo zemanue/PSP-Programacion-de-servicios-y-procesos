f817e9820c8d0b07ef89d368afae127705a37858
zemanue <manureco.97@gmail.com>
2024-11-03 21:26:33 +0100

    Update BufferedReaderYWriterCopiaArchivo.java
    Perfect copy achieved: the BufferedReader reads each line in the original file and the BufferedWriter writes that line in the copy file and goes to the next one until there are no more lines.

--- src/Repaso/BufferedReaderYWriterCopiaArchivo.java
+++ src/Repaso/BufferedReaderYWriterCopiaArchivo.java
@@ -5,12 +5,35 @@
 
 package Repaso;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class BufferedReaderYWriterCopiaArchivo {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
 
+        System.out.println("Copiando original.txt a copia.txt");
 
+        File archivoOriginal = new File("src\\Repaso\\original.txt");
+        File archivoCopia = new File("src\\Repaso\\copia.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(archivoOriginal)); // Esto lee el archivo original
+        BufferedWriter bw = new BufferedWriter(new FileWriter(archivoCopia)); // Esto escribe en la copia
+
+        String linea = br.readLine(); // Se guarda la primera línea del archivo original
+        while (linea != null) { // Mientras haya líneas en el archivo original...
+            bw.write(linea); // Se escribe la línea en la copia
+            bw.newLine(); // Se añade un salto de línea
+            linea = br.readLine(); // Se lee la siguiente línea (si no hay más líneas, "linea" será = null)
+        }
+
+        System.out.println("Archivo copiado con éxito.");
+
+        br.close();
+        bw.close();
 
     }
 }
