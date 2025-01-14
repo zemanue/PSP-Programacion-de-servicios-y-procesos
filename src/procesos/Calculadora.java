85fbab5e95011cc36a9144def3e22ce4195e9688
zemanue <manureco.97@gmail.com>
2025-01-14 10:57:58 +0100

    Create Calculadora.java
    Simple calculator, necessary for the next program

---
+++ src/procesos/Calculadora.java
@@ -0,0 +1,26 @@
+package procesos;
+
+public class Calculadora {
+    public static String calcula(double n1, double n2, String operacion) {
+        switch (operacion) {
+            case "suma":
+                return "El resultado de la suma es: " + (n1 + n2);
+            case "resta":
+                return "El resultado de la resta es: " + (n1 - n2);
+            case "multiplicacion":
+                return "El resultado de la multiplicación es: " + (n1 * n2);
+            case "division":
+                return "El resultado de la división es: " + (n1 / n2);
+            default:
+                return "Operación no válida.";
+        }
+    }
+
+    public static void main(String[] args) {
+        double n1 = Double.parseDouble(args[0]);
+        double n2 = Double.parseDouble(args[1]);
+        String operacion = args[2];
+
+        System.out.println(calcula(n1, n2, operacion));
+    }
+}
